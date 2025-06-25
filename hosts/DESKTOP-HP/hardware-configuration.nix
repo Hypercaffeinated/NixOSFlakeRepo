d66e8a6e8aef2c49fbff58d07bb646ff1c29d976
Hypercaffeinated <34581596+Hypercaffeinated@users.noreply.github.com>
2025-06-25 18:41:49 +0200

    Add files via upload

---
+++ hosts/DESKTOP-HP/hardware-configuration.nix
@@ -0,0 +1,57 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+  
+  boot = {
+    initrd = {
+      availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "ums_realtek" "uas" "sd_mod" ];
+      kernelModules = [ ];
+    };
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXROOT";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "dev/disk/by-label/NIXBOOT";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+    "/home" = {
+      device = "dev/disk/by-label/NIXHOME";
+      fsType = "btrfs";
+    };
+  };
+
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 8*1024;
+    }
+  ];
+
+  networking = {
+    useDHCP = lib.mkDefault true;
+  };  
+  
+  nixpkgs = {
+    hostPlatform = lib.mkDefault "x86_64-linux";
+  };
+  
+  hardware = {
+    cpu = {
+      intel = {
+        updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+      };
+    };
+  };
+
+}
