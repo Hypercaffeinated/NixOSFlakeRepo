baeeed91b58d5bfdaf76c358ca009aa141fe3835
Hypercaffeinated <34581596+Hypercaffeinated@users.noreply.github.com>
2025-06-26 16:27:05 +0200

    Update services.nix

--- modules/cosmic/services.nix
+++ modules/cosmic/services.nix
@@ -3,6 +3,31 @@
 {
 
   services = {
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "de";
+      };
+    };
+    printing = {
+      enable = true;
+    };
+    pipewire = {
+      enable = true;
+      wireplumber = {
+        enable = true;
+      };
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse = {
+        enable = true;
+      };
+      jack = {
+        enable = true;
+      };
+    };
     displayManager = {
       cosmic-greeter = {
         enable = true;
