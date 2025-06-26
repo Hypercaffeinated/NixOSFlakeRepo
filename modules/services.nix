d4154c73ebbe4cafe1444737b7271edd36e1908f
Hypercaffeinated <34581596+Hypercaffeinated@users.noreply.github.com>
2025-06-26 16:26:16 +0200

    Update services.nix

--- modules/services.nix
+++ modules/services.nix
@@ -3,31 +3,6 @@
 {
 
   services = {
-    xserver = {
-      enable = true;
-      xkb = {
-        layout = "de";
-      };
-    };
-    printing = {
-      enable = true;
-    };
-    pipewire = {
-      enable = true;
-      wireplumber = {
-        enable = true;
-      };
-      alsa = {
-        enable = true;
-        support32Bit = true;
-      };
-      pulse = {
-        enable = true;
-      };
-      jack = {
-        enable = true;
-      };
-    };
     swapspace = {
       enable = true;
     };
