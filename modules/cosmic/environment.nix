8eb72804c55e71acc30f4e628c1fadbad518faef
Hypercaffeinated <34581596+Hypercaffeinated@users.noreply.github.com>
2025-06-26 18:17:39 +0200

    Update environment.nix

--- modules/cosmic/environment.nix
+++ modules/cosmic/environment.nix
@@ -11,9 +11,45 @@
       XCURSOR_THEME="Bibata-Modern-Classic";
     };
     systemPackages = [
-        pkgs.cosmic-ext-tweaks
+        
         pkgs.papirus-maia-icon-theme
         pkgs.papirus-folders
+
+        pkgs.cosmic-bg
+        pkgs.cosmic-osd
+        pkgs.cosmic-term
+        pkgs.cosmic-idle
+        pkgs.cosmic-edit
+        pkgs.cosmic-comp
+        pkgs.cosmic-store
+        pkgs.cosmic-randr
+        pkgs.cosmic-panel
+        pkgs.cosmic-icons
+        pkgs.cosmic-files
+        pkgs.cosmic-player
+        pkgs.cosmic-session
+        pkgs.cosmic-greeter
+        pkgs.cosmic-ext-ctl
+        pkgs.cosmic-applets
+        pkgs.cosmic-settings
+        pkgs.cosmic-launcher
+        pkgs.cosmic-protocols
+        pkgs.cosmic-wallpapers
+        pkgs.cosmic-screenshot
+        pkgs.cosmic-ext-tweaks
+        pkgs.cosmic-applibrary
+        pkgs.cosmic-design-demo
+        pkgs.cosmic-notifications
+        pkgs.cosmic-ext-calculator
+        pkgs.cosmic-settings-daemon
+        pkgs.cosmic-workspaces-epoch
+        
+        pkgs.quick-webapps
+        pkgs.examine
+        pkgs.xdg-desktop-portal-cosmic
+        pkgs.tasks
+        pkgs.oboete
+        pkgs.libcosmicAppHook
     ];
   };
   
