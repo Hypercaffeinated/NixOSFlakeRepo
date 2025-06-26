da69d32726a4e5300fb81c76e29fb82d5dc99c55
Hypercaffeinated <34581596+Hypercaffeinated@users.noreply.github.com>
2025-06-26 19:40:29 +0200

    Create chaotic.nix

---
+++ hosts/DESKTOP-HP/chaotic.nix
@@ -0,0 +1,65 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+  chaotic = {
+    mesa-git = {
+      enable = true;
+    };
+    nyx = {
+      cache = {
+        enable = true;
+      };
+      nixPath = {
+        enable = true;
+      };
+      overlay = {
+        enable = true;
+      };
+      registry = {
+        enable = true;
+      };
+    };
+    appmenu-gtk3-module = {
+      enable = true;
+    };
+  };
+  
+  environment.systemPackages = [ 
+    pkgs.zed-editor_git
+    pkgs.xdg-desktop-portal-wlr_git
+    pkgs.wlroots_git
+    pkgs.wayland_git
+    pkgs.wayland-scanner_git
+    pkgs.wayland-protocols_git
+    pkgs.vulkanPackages_latest.vulkan-volk
+    pkgs.vulkanPackages_latest.vulkan-validation-layers
+    pkgs.vulkanPackages_latest.vulkan-utility-libraries
+    pkgs.vulkanPackages_latest.vulkan-tools
+    pkgs.vulkanPackages_latest.vulkan-loader
+    pkgs.vulkanPackages_latest.vulkan-headers
+    pkgs.vulkanPackages_latest.vulkan-extension-layer
+    pkgs.vulkanPackages_latest.spirv-tools
+    pkgs.vulkanPackages_latest.spirv-cross
+    pkgs.vulkanPackages_latest.glslang
+    pkgs.vulkanPackages_latest.gfxreconstruct
+    pkgs.spirv-headers_git
+    pkgs.nyx-generic-git-update
+    pkgs.nix-lazytrees2_git
+    pkgs.mesa_git
+    pkgs.mesa32_git
+    pkgs.mangohud_git
+    pkgs.mangohud32_git
+    pkgs.libportal_git
+    pkgs.libdrm_git
+    pkgs.libdrm32_git
+    pkgs.libbpf_git
+    pkgs.latencyflex-vulkan
+    pkgs.jovian-chaotic.steam_notif_daemon
+    pkgs.jovian-chaotic.steam-unwrapped
+    pkgs.firefox_nightly
+    pkgs.bpftools_full
+    pkgs.appmenu-gtk3-module
+  ];
+  
+}
