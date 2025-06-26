6a770d67fba31172d2c77be965f6b6e48502be44
Hypercaffeinated <34581596+Hypercaffeinated@users.noreply.github.com>
2025-06-26 18:01:28 +0200

    Update environment.nix

--- modules/cosmic/environment.nix
+++ modules/cosmic/environment.nix
@@ -6,6 +6,14 @@
     sessionVariables = {
       COSMIC_DATA_CONTROL_ENABLED = 1;
     };
+    variables = {
+      
+    };
+    systemPackages = [
+        pkgs.cosmic-ext-tweaks
+        pkgs.papirus-maia-icon-theme
+        pkgs.papirus-folders
+    ];
   };
   
 }
