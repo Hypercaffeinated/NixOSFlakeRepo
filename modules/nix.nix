0fcb71d94ec04403b6195b8d191752fda0ef5a9e
Hypercaffeinated <34581596+Hypercaffeinated@users.noreply.github.com>
2025-06-26 19:10:23 +0200

    Update nix.nix

--- modules/nix.nix
+++ modules/nix.nix
@@ -4,8 +4,18 @@
   
     nix = {
       settings = {
-        substituters = [ https://install.determinate.systems];
-        trusted-public-keys = [ "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM=" ];
+        trusted-substituters = [ 
+          "https://install.determinate.systems"
+        ];
+        extra-trusted-substituters
+          "https://chaotic-nyx.cachix.org/"
+        ];
+        trusted-public-keys = [ 
+          "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
+        ];
+        extra-trusted-public-keys = [
+          "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+        ];
         experimental-features = "nix-command flakes";
         lazy-trees = true;
       };
