8c946db99424853c67a0ca34efe6741fc84683d0
Hypercaffeinated <34581596+Hypercaffeinated@users.noreply.github.com>
2025-06-26 13:13:33 +0200

    Update console.nix

--- modules/console.nix
+++ modules/console.nix
@@ -3,7 +3,7 @@
 {
 
   console = {
-    font = "latarcyrheb-sun32"; #"solar24x32" #"Lat2-Terminus16";
+    font = "sun12x22"; # latarcyrheb-sun32 # Lat2-Terminus16
     keyMap = "de";
     earlySetup = true;
   };
