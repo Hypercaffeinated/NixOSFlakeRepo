ce78704d5f1526c09a47c9e457d4a7b516d7802b
Hypercaffeinated <34581596+Hypercaffeinated@users.noreply.github.com>
2025-06-27 22:29:14 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -107,6 +107,7 @@
             ./hosts/DESKTOP-HP/chaotic.nix
             ./hosts/DESKTOP-HP/hardware-configuration.nix
             ./hosts/DESKTOP-HP/networking.nix
+            ./hosts/DESKTOP-HP/programs.nix
 
             ./modules/boot.nix
             ./modules/chaotic.nix
