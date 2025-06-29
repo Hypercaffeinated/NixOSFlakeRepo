67ce9ade6fbcda18106e277fc010175a7a742334
Hypercaffeinated <34581596+Hypercaffeinated@users.noreply.github.com>
2025-06-29 17:48:54 +0200

    Update boot.nix

--- hosts/NOTEBOOK-ACER/boot.nix
+++ hosts/NOTEBOOK-ACER/boot.nix
@@ -1,9 +1,23 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 {
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = linuxPackages_cachyos-lto; #pkgs.linuxPackages_latest;
+    zfs = {
+      package = lib.mkOverride pkgs.zfs_cachyos;
+    };
+    supportedFilesystems = [
+      "btrfs"
+      "vfat"
+      "f2fs"
+      "xfs"
+      "ntfs"
+      "cifs"
+      "bcachefs"
+      "ext4"
+      "zfs"
+    ];
   };
   
 }
